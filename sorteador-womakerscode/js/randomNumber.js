493c3c5cd9bab541292b1010fa193ee7ac0e41b4
Aline Bezzoco <contato.alinebezzoco@gmail.com>
2019-04-27 15:54:25 -0300

    Update randomNumber.js

--- sorteador-womakerscode/js/randomNumber.js
+++ sorteador-womakerscode/js/randomNumber.js
@@ -7,7 +7,7 @@ $(function(){
     $("#gogogo").click(function() {
     
         var numLow = 1;
-        var numHigh = 29;
+        var numHigh = 31;
         
         var adjustedHigh = (parseFloat(numHigh) - parseFloat(numLow)) + 1;
         
